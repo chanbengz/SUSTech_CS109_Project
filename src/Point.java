ca18010494fdb3d1d1a96f5a560caadbf036d4f3
Cerulime <1124775300@qq.com>
2022-11-11 15:53:32 +0800

    棋盘

---
+++ src/Point.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Point
+{
+    int x, y;
+    boolean show;
+    boolean alive;
+
+    public Point(int x, int y, boolean show, boolean alive) {
+        this.x = x;
+        this.y = y;
+        this.show = show;
+        this.alive = alive;
+    }
+
+    public Point(Point point) {
+        this.x = point.x;
+        this.y = point.y;
+        this.show = point.show;
+        this.alive = point.alive;
+    }
+}
