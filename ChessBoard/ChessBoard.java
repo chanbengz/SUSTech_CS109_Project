ad64895c5cf30256d21a8cdff35631fbffbf46fb
Cerulime <1124775300@qq.com>
2022-11-11 23:17:31 +0800

    remove the exception

--- ChessBoard/ChessBoard.java
+++ ChessBoard/ChessBoard.java
@@ -48,14 +48,13 @@ public class ChessBoard
         this.players[0].pieces.init(RedStart);
         this.players[1].pieces.init(BlackStart);
     }
-    int Move(Point piece,Point dest) throws Exception {
+    int Move(Point piece,Point dest)
+    {
         if(piece.x==dest.x && piece.y==dest.y)
         {
             piece.show=true;
             return 0;
         }
-        if(piece.level==6 && dest.level==1)return dest.level;
-        if(piece.level>dest.level || (dest.level==7 && piece.level==6))throw new Exception();
         return dest.level;
     }
     void InitialMap(int player)
@@ -87,17 +86,16 @@ public class ChessBoard
                 else System.out.printf("%2d ",0);
             }
     }
-    void Input(Point[] fun) throws Exception {
+    void Input(Point[] fun)
+    {
         Scanner input=new Scanner(System.in);
         System.out.println("Input: src dest");
         int x1=input.nextInt(),y1=input.nextInt(),x2=input.nextInt(),y2=input.nextInt();
-        if(map[x1][y1].player==-1)throw new Exception();
         fun[0]=this.players[map[x1][y1].player].pieces.chess[map[x1][y1].index];
         if(map[x2][y2].player==-1)
             fun[1]=new Point(x2,y2,0,true,true);
         else
             fun[1]=this.players[map[x2][y2].player].pieces.chess[map[x2][y2].index];
-        if((x1!=x2 || y1!=y2) && fun[0].level==fun[1].level)throw new Exception();
     }
     void Scoring(int player,int cost)
     {
@@ -126,15 +124,7 @@ public class ChessBoard
             Point[] fun=new Point[2];
             for(int i=0;i<=1;i++)
                 fun[i]=new Point(0,0,0,false,false);
-            try
-            {
-                Input(fun);
-            }
-            catch (Exception e)
-            {
-                System.out.println("Invalid Input");
-                break;
-            }
+            Input(fun);
             if(fun[0].level==7)
             {
                 if(fun[0]==fun[1])
@@ -143,24 +133,6 @@ public class ChessBoard
                 }
                 else
                 {
-                    int x=fun[0].x,y=fun[0].y,tot=0;
-                    if(x==fun[1].x)
-                    {
-                        int dy=y<fun[1].y ? 1 : -1;
-                        for(int j=y+1;j<=fun[1].y-1;j+=dy)
-                            if(map[x][j].player!=-1)tot++;
-                    }
-                    else
-                    {
-                        int dx=x<fun[1].x ? 1 : -1;
-                        for(int i=x+1;i<=fun[1].x-1;i+=dx)
-                            if(map[i][y].player!=-1)tot++;
-                    }
-                    if(tot!=1)
-                    {
-                        System.out.println("Invalid input");
-                        break;
-                    }
                     map[fun[1].x][fun[1].y].player=-1;
                     int cost=fun[1].level;
                     Scoring(turn,cost);
@@ -168,22 +140,14 @@ public class ChessBoard
             }
             else
             {
-                try
-                {
-                    int cost=Move(fun[0],fun[1]);
-                    int TmpPlayer=map[fun[0].x][fun[0].y].player;
-                    int TmpIndex=map[fun[0].x][fun[0].y].index;
-                    map[fun[0].x][fun[0].y].player=-1;
-                    map[fun[1].x][fun[1].y].player=TmpPlayer;
-                    map[fun[1].x][fun[1].y].index=TmpIndex;
-                    this.players[turn].pieces.chess[map[fun[1].x][fun[1].y].index]=fun[0];
-                    Scoring(turn,cost);
-                }
-                catch (Exception e)
-                {
-                    System.out.println("Invalid Input");
-                    break;
-                }
+                int cost=Move(fun[0],fun[1]);
+                int TmpPlayer=map[fun[0].x][fun[0].y].player;
+                int TmpIndex=map[fun[0].x][fun[0].y].index;
+                map[fun[0].x][fun[0].y].player=-1;
+                map[fun[1].x][fun[1].y].player=TmpPlayer;
+                map[fun[1].x][fun[1].y].index=TmpIndex;
+                this.players[turn].pieces.chess[map[fun[1].x][fun[1].y].index]=fun[0];
+                Scoring(turn,cost);
             }
             turn^=1;
             //SavePoint();
