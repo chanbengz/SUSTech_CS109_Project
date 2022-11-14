94a2db032b28527babe77baf3a48a9bdbf6a354a
Cerulime <1124775300@qq.com>
2022-11-14 12:10:22 +0800

    crypt

--- ChessBoard/FileOperation.java
+++ ChessBoard/FileOperation.java
@@ -1,23 +1,54 @@
 package ChessBoard;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.UUID;
+
 public class FileOperation
 {
+    static String Encrypt(String data, UUID uuid) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        String password=uuid.toString();
+        KeyGenerator gen=KeyGenerator.getInstance("AES");
+        gen.init(128,new SecureRandom(password.getBytes()));
+        SecretKey secretKey=gen.generateKey();
+        SecretKeySpec key=new SecretKeySpec(secretKey.getEncoded(), "AES");
+        Cipher cipher=Cipher.getInstance("AES");
+        cipher.init(Cipher.ENCRYPT_MODE,key);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(data.getBytes()));
+    }
+    static String Decrypt(String data, UUID uuid)
+    {
+
+    }
     public static String SaveGame(ChessBoard game) throws IOException {
+        StringBuilder data= new StringBuilder();
+        data.append(game.uuid.toString()).append(Player.pause);
+        data.append(game.players[0].Msg()).append(game.players[1].Msg());
+        int n=game.opt_stack.size();
+        data.append(n).append(Player.pause);
+        for(Operation tmp:game.opt_stack)
+            data.append(tmp.transfer()).append(Player.pause);
         String dir="Save/";
-        dir+=game.uuid.toString();
-        dir+=".chess";
-        File file=new File(dir);
+        File dictionary=new File(dir);
+        if(!dictionary.exists())
+            if(!dictionary.mkdir())
+                throw new IOException();
+        String dirFile=dir+game.uuid.toString()+".chess";
+        File file=new File(dirFile);
         if(!file.exists())
             if(!file.createNewFile())
                 throw new IOException();
         FileOutputStream fos=new FileOutputStream(file);
-        fos.write((game.uuid.toString()+Player.pause).getBytes());
-        for(int i=0;i<=1;i++)
-            fos.write(game.players[i].Msg().getBytes());
-        int n=game.opt_stack.size();
-        fos.write((n+Player.pause).getBytes());
-        for(Operation tmp:game.opt_stack)
-            fos.write((tmp.transfer()+Player.pause).getBytes());
+        try {
+            fos.write((Encrypt(data.toString(),game.uuid)).getBytes());
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
         fos.close();
         return dir;
     }
@@ -27,15 +58,20 @@ public class FileOperation
         if(!dictionary.exists())
             if(!dictionary.mkdir())
                 throw new IOException();
-        dir+=Alice.id+".usr";
-        File file=new File(dir);
+        String dirFile=dir+Alice.id+".usr";
+        File file=new File(dirFile);
         if(!file.exists())
             if(!file.createNewFile())
                 throw new IOException();
         FileOutputStream fos=new FileOutputStream(file);
-        fos.write(Alice.UserMsg().getBytes());
+        try {
+            fos.write(Encrypt(Alice.UserMsg(),Alice.uuid).getBytes());
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException |
+                 BadPaddingException e) {
+            throw new RuntimeException(e);
+        }
         fos.close();
-        return dir;
+        return dirFile;
     }
     public static String Load(String dir) throws IOException {
         File file=new File(dir);
@@ -45,6 +81,10 @@ public class FileOperation
         while(fis.read(buf,0, buf.length)!=-1)
             input.append(new String(buf));
         fis.close();
-        return input.toString();
+        String name=dir.substring(dir.lastIndexOf("/")+1);
+        UUID uuid;
+        if(name.contains(".chess"))uuid=UUID.fromString(name.substring(0,name.indexOf(".chess")));
+        else uuid=UUID.nameUUIDFromBytes(name.substring(0,name.indexOf(".usr")).getBytes());
+        return Decrypt(input.toString(),uuid);
     }
 }
--- ChessBoard/Player.java
+++ ChessBoard/Player.java
@@ -1,5 +1,7 @@
 package ChessBoard;
 import java.util.ArrayList;
+import java.util.UUID;
+
 public class Player
 {
     public ChessPieces pieces=new ChessPieces();
@@ -7,16 +9,19 @@ public class Player
     public String id;
     public int rating=1500;
     public boolean isAI=false;
+    UUID uuid;
     public ArrayList<String> history=new ArrayList<>();
     public static String pause="?!-.";
     public Player()
     {
         id="Player";
+        uuid=UUID.nameUUIDFromBytes(id.getBytes());
     }
     public Player(String userid,boolean isAI)
     {
         id=userid;
         this.isAI=isAI;
+        uuid=UUID.nameUUIDFromBytes(id.getBytes());
     }
     public Player(Player user)
     {
@@ -24,18 +29,17 @@ public class Player
         this.score=user.score;
         this.id=user.id;
         this.isAI=user.isAI;
+        this.uuid=user.uuid;
         this.rating=user.rating;
         this.history.addAll(user.history);
     }
     public String Msg()
     {
-        String out="";
-        out+=id+pause+isAI+pause+rating+pause+pieces.Msg();
-        return out;
+        return id+pause+isAI+pause+rating+pause+pieces.Msg();
     }
     public String UserMsg()
     {
-        StringBuilder out= new StringBuilder(id+pause+rating+pause);
+        StringBuilder out= new StringBuilder(id+pause+uuid.toString()+pause+rating+pause);
         out.append(history.size()).append(pause);
         for(String game:history)
             out.append(game).append(pause);
@@ -55,6 +59,9 @@ public class Player
         int pos_id=input.indexOf(pause,index);
         id=input.substring(index,pos_id);
         index=pos_id+pause.length();
+        int pos_uuid=input.indexOf(pause,index);
+        uuid=UUID.fromString(input.substring(index,pos_uuid));
+        index=pos_uuid+pause.length();
         int pos_rat=input.indexOf(pause,index);
         rating=Integer.parseInt(input.substring(index,pos_rat));
         index=pos_rat+pause.length();
